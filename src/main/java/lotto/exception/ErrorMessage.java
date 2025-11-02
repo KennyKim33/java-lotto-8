7782322d85cbd7b303955933f2abbab189a873b2
KennyKim33 <kennykimm12@gmail.com>
2025-11-02 23:08:42 +0900

    feat: 로또 검증을 위한 에러 메시지 추가
    - 숫자 6개에 대한 에러 메시지

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,10 +1,16 @@
 package lotto.exception;
 
+import lotto.LottoConfig;
+
 public enum ErrorMessage {
     NOT_EMPTY("입력은 비어 있을 수 없습니다."),
     NOT_NUMERIC("숫자만 입력 가능합니다."),
     INVALID_LOTTO_FORMAT("로또 번호는 쉼표(,)로 구분하여 입력해주세요. (예: 1,2,3,4,5,6)"),
-    INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다.");
+    INVALID_LOTTO_SIZE(String.format("로또 번호는 %d개 여야 합니다.", LottoConfig.LOTTO_SIZE)),
+    INVALID_LOTTO_RANGE(String.format("로또 번호는 %d부터 %d사이의 숫자여야합니다."
+            , LottoConfig.MIN_NUMBER, LottoConfig.MAX_NUMBER)),
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되면 안됩니다.");
+
     private static final String PREFIX = "[ERROR] ";
     private final String message;
 
