513ab4f5aa38f2990c7fb79e903aeec955640c8a
KennyKim33 <kennykimm12@gmail.com>
2025-11-03 00:42:12 +0900

    feat(ErrorMessage): 보너스 번호와 로또 번호의 중복에 관한 예외 메시지 구현

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -6,10 +6,12 @@ public enum ErrorMessage {
     NOT_EMPTY("입력은 비어 있을 수 없습니다."),
     NOT_NUMERIC("숫자만 입력 가능합니다."),
     INVALID_LOTTO_FORMAT("로또 번호는 쉼표(,)로 구분하여 입력해주세요. (예: 1,2,3,4,5,6)"),
-    INVALID_LOTTO_SIZE(String.format("로또 번호는 %d개 여야 합니다.", LottoConfig.LOTTO_SIZE)),
+    INVALID_LOTTO_SIZE(String.format("로또 번호는 %d개 여야 합니다.",
+            LottoConfig.LOTTO_SIZE)),
     INVALID_LOTTO_RANGE(String.format("로또 번호는 %d부터 %d사이의 숫자여야합니다."
             , LottoConfig.MIN_NUMBER, LottoConfig.MAX_NUMBER)),
-    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되면 안됩니다.");
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되면 안됩니다."),
+    BONUS_DUPLICATED_WITH_WINNING("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
