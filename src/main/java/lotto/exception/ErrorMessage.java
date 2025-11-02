419973c0233bc016ad06e2d4bd819ed009ae6c1e
KennyKim33 <kennykimm12@gmail.com>
2025-11-03 01:25:06 +0900

    feat(ErrorMessage): 최소 금액에 대한 예외 메시지 추가
    - 최소 금액이 1000원 이상인가
    - 금액의 단위가 1000원인가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -11,7 +11,11 @@ public enum ErrorMessage {
     INVALID_LOTTO_RANGE(String.format("로또 번호는 %d부터 %d사이의 숫자여야합니다."
             , LottoConfig.MIN_NUMBER, LottoConfig.MAX_NUMBER)),
     DUPLICATE_LOTTO_NUMBER("로또 번호는 중복되면 안됩니다."),
-    BONUS_DUPLICATED_WITH_WINNING("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    BONUS_DUPLICATED_WITH_WINNING("보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    INVALID_AMOUNT_MINIMUM(String.format("구매 금액은 %,d원 이상이어야 합니다."
+            , LottoConfig.TICKET_UNIT)),
+    INVALID_AMOUNT_UNIT(String.format("구매 금액은 %,d원 단위로 입력해야 합니다.",
+            LottoConfig.TICKET_UNIT));
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
